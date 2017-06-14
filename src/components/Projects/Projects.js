64a64a5e2faf7602662d954bc53e95c3b8420fe9
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-14 09:39:44 -0600

    added project groups and updated available links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ export const Projects = () => {
           <p>•</p>
           <p>Finding myself without employment, a challenging routine and knowing that I had to begin my search for livlihood, I began to reflect upon my years as a chemcial engineer.  As my reflections came to the surface of my mind, it occurred to me that the times that I was the most passionate and engaged throughout my career were the times that I was involved in programming computers.</p>
           <p>•</p>
-          <p>It was at this point in my realization that I enrolled at Turing School of Software and Design to become a programmer.  I began school late January of 20017.  Here you will find a list of projects that have resulted from my studies.</p>
+          <p>It was at this point in my realization that I enrolled at Turing School of Software and Design to become a programmer.  I began school late January of 2017.  Here you will find a list of projects that have resulted from my studies.</p>
           <p>Please enjoy.</p>
         </article>
       </div>
@@ -27,17 +27,17 @@ export const Projects = () => {
       <div id='project-links-2'>
         <img className='project-badge' id='badge-2' src={require('../../assets/images/Projects/project-badge2.svg')} />
         <div className='link-box link-box2'>
-          <a className='p-links first-link' target='blank' href='https://i-like-star-wars.herokuapp.com/'>SWApi Box</a>
-          <a className='p-links' target='blank' href='https://jennpeavler.github.io/gameTime/'>Breakout</a>
-          <a className='p-links last-link' target='blank' href='https://i-like-star-wars.herokuapp.com/'>PinShow</a>
+          <a className='p-links first-link' target='blank' href='https://i-like-star-wars.herokuapp.com/'>Movie Tracker</a>
+          <a className='p-links' target='blank' href='https://jennpeavler.github.io/gameTime/'>Weatherly</a>
+          <a className='p-links last-link' target='blank' href='https://i-like-star-wars.herokuapp.com/'>Health Assist</a>
         </div>
       </div>
       <div id='project-links-3'>
         <img className='project-badge' id='badge-3' src={require('../../assets/images/Projects/project-badge4.svg')} />
         <div className='link-box link-box3'>
-          <a className='p-links first-link links-3' target='blank' href='https://i-like-star-wars.herokuapp.com/'>static kitty</a>
-          <a className='p-links links-3' target='blank' href='https://jennpeavler.github.io/gameTime/'>static cat</a>
-          <a className='p-links last-link links-3' target='blank' href='https://i-like-star-wars.herokuapp.com/'>static meow</a>
+          <a className='p-links first-link links-3' target='blank' href='https://jennpeavler.github.io/jp-comp-challenge-3/'>static kitty</a>
+          <a className='p-links links-3' target='blank' href='https://jennpeavler.github.io/jp-comp-challenge-2/'>static cat</a>
+          <a className='p-links last-link links-3' target='blank' href='https://jennpeavler.github.io/jp-comp-challenge-1/'>static meow</a>
         </div>
       </div>
     </section>
