610f839714d42dc70dde4869f529d5867e89f8cd
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-05 20:26:13 -0600

    added sme site and link

--- src/components/Sme/Sme.js
+++ src/components/Sme/Sme.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Sme = () => {
   return (
     <section id='lullaby-bg'>
-      <header id='lullaby-header'>
+      {/* <header id='lullaby-header'>
         <img id='lullaby-pic' src={require('../../assets/images/Lullaby/lullaby.png')} />
         <nav id='lullaby-nav'>
           <Link id='lullaby-home' to='/'>
@@ -18,15 +18,18 @@ export const Sme = () => {
           <img className='lullaby-media' id='lullaby-instagram' src={require('../../assets/images/Lullaby/instagram.svg')} />
           <img className='lullaby-media' id='lullaby-mail' src={require('../../assets/images/Lullaby/catmail.svg')} />
         </nav>
-      </header>
+      </header> */}
       <main id='lullaby-main'>
         <section>
+          <Link id='sme-home' to='/'>
+            <p id='sme-nav-link'>HOME</p>
+          </Link>
           <img id='sme-collage' src={require('../../assets/images/Sme/SmeBackground.jpg')} />
         </section>
       </main>
-      <footer>
+      {/* <footer>
         <p id='lullaby-copyright'>All Rights Reserved to everyone.  Thank you to all the interweb cat Lovers.  Lullaby Inc å @2017.</p>
-      </footer>
+      </footer> */}
     </section>
   )
 }
