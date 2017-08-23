27c7add6dc37404a139a2af220c9a71162ee1de3
Jennifer Peavler <jenn.peavler@gmail.com>
2017-08-23 17:16:13 -0600

    add intro section.  move family section to bottom

--- src/components/Family/Family.js
+++ src/components/Family/Family.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export const Family = () => {
 return (
     <section id='section1'>
-      <h1 id='hi'>Hello I am <span className='bigBold'>Jenn Peavler</span></h1>
+      <h1 id='family-hi'><span className='bigBold'>Click</span> on the <span className='bigBold'>circles</span> if you want to...</h1>
       <section id='link-section'>
         <div className='section1-div' id='div1-1'>
-          <h2>Come with me on a journey.</h2>
+          <h2>meet my family...</h2>
           <Link to='/me'>
             <input type='image' id='sme-img' className='circle-images' src={require('../../assets/images/Family/jenn.jpg')} />
           </Link>
@@ -17,12 +17,12 @@ return (
             <input type='image' className='circle-images' id='dragon-img' src={require('../../assets/images/Family/dragon.jpg')} />
           </Link>
         </div>
-        <div className='section1-div' id='div1-3'>
+        <div className='section1-div' id='family-div1-3'>
           <Link to='/lullaby'>
             <input type='image' className='circle-images' id='lullaby-img' src={require('../../assets/images/Family/lullaby.jpg')} />
           </Link>
-          <img className='quote-swirl' src={require('../../assets/images/Family/quote-swirl.svg')} />
-          <p id='quote'>“I have only one heart, I will not hide it from the world”</p>
+          {/* <img className='quote-swirl' src={require('../../assets/images/Family/quote-swirl.svg')} /> */}
+          {/* <p id='quote'>“I have only one heart, I will not hide it from the world”</p> */}
         </div>
         <div className='section1-div' id='div1-4'>
           <Link to='/dexter'>
@@ -30,7 +30,7 @@ return (
           </Link>
         </div>
       </section>
-      <p id='intro'><span className='bigBold'>Family</span> is very important to me so to show a bit of my creative side, I have made bios inspired by me and my family.  Click on these images for a bit of silliness and to get to know me and my family!  The bio pages are static, but they are still fun to look at and read.</p>
+      <p id='family-intro'><span className='bigBold'>Family</span> is very important to me so to show a bit of my creative side, I have made bios inspired by me and my family.  Click on these images for a bit of silliness and to get to know me and my family!  The bio pages are static, but they are still fun to look at and read.</p>
     </section>
   )
 }
--- src/components/MainWrapper/MainWrapper.js
+++ src/components/MainWrapper/MainWrapper.js
@@ -1,19 +1,24 @@
 import React from 'react'
 
+import { Intro } from '../Intro/Intro'
 import { Family } from '../Family/Family'
 import { About } from '../About/About'
 import { Projects } from '../Projects/Projects'
 import { SocialMedia } from '../SocialMedia/SocialMedia'
 import { FunStuff } from '../FunStuff/FunStuff'
+import { ThankYou } from '../ThankYou/ThankYou'
 
 export const MainWrapper = () => {
   return (
     <section>
-      <Family />
+      <Intro />
       <About />
       <Projects />
       <SocialMedia />
-      <FunStuff />
+      {/* <Projects /> */}
+      <Family />
+      {/* <FunStuff /> */}
+      <ThankYou />
     </section>
   )
 }
